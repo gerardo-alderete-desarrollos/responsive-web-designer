4dd444045173d7cce02b73b22a47f63bdb493c6d
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-12-26 22:45:37 -0600

    TRABAJANDO CON VARIABLES CSS

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-/* 
-    CSSOM
-    CSS Object Model 
-    window.getComputedStyle(elemento).getPropertyValue() // lee los estilos de un elemento
+// /* 
+//     CSSOM
+//     CSS Object Model 
+//     window.getComputedStyle(elemento).getPropertyValue() // lee los estilos de un elemento
 
-    element.style // todo elemento del DOM tiene la propiedad style
- */
+//     element.style // todo elemento del DOM tiene la propiedad style
+//  */
 
-//  document.body.style.backgroundColor = 'red';
- document.body.classList.add('red');
+// //  document.body.style.backgroundColor = 'red';
+//  document.body.classList.add('red');
 
- const color = 'red';
- const size = 2;
- const myStyle = `
-    background: ${color};
-    color: white;
-    margin-top: 4em;
-    border-bottom: ${size * 2}px solid black;
- `
+//  const color = 'red';
+//  const size = 2;
+//  const myStyle = `
+//     background: ${color};
+//     color: white;
+//     margin-top: 4em;
+//     border-bottom: ${size * 2}px solid black;
+//  `
 
- //1. Sobreescribiendo
-// document.getElementById('title').setAttribute('style', myStyle);
+//  //1. Sobreescribiendo
+// // document.getElementById('title').setAttribute('style', myStyle);
 
-const setStyles = (element, styles) => {
-    element.setAttribute('style', styles);
-}
+// const setStyles = (element, styles) => {
+//     element.setAttribute('style', styles);
+// }
 
-setStyles(document.getElementById('title'), myStyle);
+// setStyles(document.getElementById('title'), myStyle);
 
 
-//2.- Agregando a los estilos inline existentes
-// document.getElementById('title').style += `;${myStyle}`;
+// //2.- Agregando a los estilos inline existentes
+// // document.getElementById('title').style += `;${myStyle}`;
+
+//Modificar variables
+document.documentElement.style.setProperty('--name', 'Beto');
+
+//Leer variables
+console.log(getComputedStyle(document.documentElement).getPropertyValue('--name'));
