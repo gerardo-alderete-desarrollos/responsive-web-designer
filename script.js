0c1642949fc8e0535769672039a1616e3da356b1
Gerardo Alderete <gerardoalderete92@gmail.com>
2018-12-26 22:27:28 -0600

    PROPIEDAD STYLE

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const getScrollBarWidth  = () => innerWidth - document.documentElement.clientWidth
+/* 
+    CSSOM
+    CSS Object Model 
+    window.getComputedStyle(elemento).getPropertyValue() // lee los estilos de un elemento
 
-document.documentElement.style.setProperty('--scroll-bar', getScrollBarWidth())
+    element.style // todo elemento del DOM tiene la propiedad style
+ */
+
+//  document.body.style.backgroundColor = 'red';
+ document.body.classList.add('red');
+
+ const color = 'red';
+ const size = 2;
+ const style = `
+    background: ${color};
+    color: white;
+    margin-top: 4em;
+    border-bottom: ${size * 2}px solid black;
+ `
+
+ //1. Sobreescribiendo
+document.getElementById('title').setAttribute('style', style);
